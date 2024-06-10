3489cc6415408be08c6d71b07dc0eed1caa9de95
bluml <blumluan@gmail.com>
2024-06-10 10:11:45 +0200

    base

---
+++ app/src/main/java/com/example/lingua/data/model/Item.kt
@@ -0,0 +1,3 @@
+package com.example.lingua.data.model
+
+data class Item(val title: String, val text: String)
--- app/src/main/java/com/example/lingua/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/lingua/ui/home/HomeFragment.kt
@@ -1,38 +1,43 @@
 package com.example.lingua.ui.home
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.lingua.R
+import com.example.lingua.data.adapter.MyAdapter
+import com.example.lingua.data.model.Item
 import com.example.lingua.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return binding.root
+    }
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        val itemList = listOf(
+            Item("hey", "Item #2"),
+            Item("Title #3", "Item #3"),
+            Item("Title #4", "Item #4"),
+
+        )
+
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        binding.recyclerView.adapter = MyAdapter(itemList)
     }
 
     override fun onDestroyView() {
